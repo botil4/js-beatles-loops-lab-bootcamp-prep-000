79cf390483137e64d892d3ea0c3ff1a09b58b07d
Travis Clarkson <tk1667@gmail.com>
2018-02-05 02:39:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var array = []
   
   for (let i = 0; i < musicians.length; i++) {
-    array = array.push(`${musicians} plays ${instruments}`)
+    array.push(`${musicians} plays ${instruments}`)
   }
   return array
 }
